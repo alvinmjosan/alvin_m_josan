bc54e80dd128c49717e4cdd5dca83cc80d7cb846
alvinmjosan <alvinmjosan@gmail.com>
2026-01-19 09:08:14 +0530

    added ArrayListExample.java

---
+++ ArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class ArrayListExample{
+    public static void main(String[]args){
+        ArrayList <String> items=new ArrayList<String>();
+        items.add("a");
+        items.add("l");
+        items.add("v");
+        items.add("i");
+        items.add("n");
+        System.out.println(items);
+    }
+}
